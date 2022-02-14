17930232564f90516f5a5203b4cb1bb9fe9e5923
YaoyuanZhang15 <70974616+YaoyuanZhang15@users.noreply.github.com>
2022-02-14 16:43:35 -0600

    Clouds

--- MissonDemolition-uinity/Assets/script/Follow.cs
+++ MissonDemolition-uinity/Assets/script/Follow.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
     static public GameObject POI;
+    [Header("Set in Inspecter")]
+    public float easing = 0.05f;
+
+    public Vector2 minXY = Vector2.zero;
+
+    [Header("Set Dynamically")]
     public float camz;
     public void Awake()
     {
@@ -19,9 +25,16 @@ public class Follow : MonoBehaviour
     }
 
         Vector3 destination = POI.transform.position;
-        
+
+        destination.x = Mathf.FloatToHalf().Max(minXY.x, destination.x);
+        destination.y = Mathf.FloatToHalf().Max(minXY.x, destination.y);
+
+        destination = Vector3.lerp(Transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+
+
+        Camera.main.orthographicSize = destination.y +10;
 
 
 
